e37dfa2f396fcbedad17298fd5e287473970df0c
meow1928 <mark.styx@hotmail.com>
2021-06-01 12:35:22 -0400

    updates

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages,setup
 setup(
     name='pyDatorama',
     packages=find_packages(),
-    version='0.10',
+    version='0.11',
     description="A Python API wrapping library for Salesforce's datorama platform.",
     author='meow1928',
     install_requires=['pandas']
